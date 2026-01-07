06e70f0dc03feff634f2bd4932ba934edf0fa0ca
kokchun <kokchungiang@gmail.com>
2026-01-07 11:27:48 +0100

    data models

--- code-alongs/08_fastapi_crud/data_processing.py
+++ code-alongs/08_fastapi_crud/data_processing.py
@@ -3,6 +3,7 @@ import json
 from pprint import pprint
 from pydantic import BaseModel, Field
 
+
 # convenience/utility function to read a json file
 def read_json(filename):
     with open(DATA_PATH / filename) as file:
@@ -10,17 +11,42 @@ def read_json(filename):
 
     return data
 
+
 # data models
+
 
 class Book(BaseModel):
     id: int
-    title: str 
-    author: str 
-    year: int
+    title: str
+    author: str
+    year: int = Field(gt=1000, lt=2027, description="Year when book was published")
     description: str
 
+
 class Library(BaseModel):
-    name: str 
+    name: str
     books: list[Book]
+
+
+# deserialize json data into pydantic models
+def library_data(filename):
+    json_data = read_json(filename)
+
+    Library(
+        name="Coolu libraru",
+        books=[
+            {
+                "author": "Douglas Adams",
+                "description": "A humorous science‑fiction adventure following an "
+                "ordinary man through absurd and unexpected cosmic "
+                "events.",
+                "id": 1,
+                "title": "The Hitchhiker's Guide to the Galaxy",
+                "year": 1979,
+            }
+        ],
+    )
+    return Library(**json_data)
+
 
 pprint(read_json("library.json"))
