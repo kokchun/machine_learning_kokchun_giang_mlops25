50c47e793629f9dda85a5fc24c0bdc1e7ab1cf32
kokchun <kokchungiang@gmail.com>
2026-01-07 11:35:52 +0100

    ready to build api

--- code-alongs/08_fastapi_crud/api.py
+++ code-alongs/08_fastapi_crud/api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from data_processing import library_data
+
+books = library_data("library.json").books
 
 app = FastAPI()
 
 @app.get("/books")
 async def read_books():
-    return {"book": "Hello API books"}
+    return books
