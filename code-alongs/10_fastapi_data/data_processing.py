124e2084f566d3277bc5317f6e6da18c37711e77
kokchun <kokchungiang@gmail.com>
2026-01-12 11:12:25 +0100

    summary

--- code-alongs/10_fastapi_data/data_processing.py
+++ code-alongs/10_fastapi_data/data_processing.py
@@ -1,26 +1,30 @@
 from constants import DATA_PATH
-import pandas as pd 
+import pandas as pd
 from pprint import pprint
 import json
 
 df = pd.read_csv(DATA_PATH / "Sales.csv")
 
+
 class DataExplorer:
-    def __init__(self, limit = 100):
+    def __init__(self, limit=100):
         self._df = df.head(limit)
         self._df_full = df
 
     @property
     def df(self):
         return self._df
-    
+
     def summary(self):
-        self._df = self._df_full.describe()
-        return self # allows for method chaining
+        self._df = (
+            self._df_full.describe().T.drop(["count"], axis=1).drop(["Day", "Year"])
+        )
+        return self  # allows for method chaining
 
     def json_response(self):
         json_data = self.df.to_json(orient="records")
         return json.loads(json_data)
+
 
 if __name__ == "__main__":
     data_explorer = DataExplorer(limit=2)
