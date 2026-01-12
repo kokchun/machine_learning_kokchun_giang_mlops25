c22def8d0c8cb73c5512f40ebb762107280817a5
kokchun <kokchungiang@gmail.com>
2026-01-12 09:45:06 +0100

    data processing

--- code-alongs/10_fastapi_data/data_processing.py
+++ code-alongs/10_fastapi_data/data_processing.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 df = pd.read_csv(DATA_PATH / "Sales.csv")
 
+class DataExplorer:
+    def __init__(self, limit = 100):
+        self._df = df.head(limit)
+
+    @property
+    def df(self):
+        return self._df
 
 if __name__ == "__main__":
-    print(df)
+    data_explorer = DataExplorer(limit=5)
+    print(data_explorer.df)
