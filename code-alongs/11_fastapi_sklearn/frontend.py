40300f048c3f8088c67dbc0bf3fb6237e1d536bf
kokchun <kokchungiang@gmail.com>
2026-01-14 14:31:07 +0100

    frontend

---
+++ code-alongs/11_fastapi_sklearn/frontend.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import httpx
+
+URL = "http://127.0.0.1:8000/api/predict"
+
+st.markdown("# Predict iris flower")
+st.markdown("App to predict an Iris flower, take some measurement on your flower")
+
+with st.form("iris_data"):
+    sepal_length = st.number_input(
+        "Sepal length (cm)", min_value=3.8, max_value=8.4, value=6.0
+    )
+    sepal_width = st.number_input(
+        "Sepal width (cm)", min_value=1.5, max_value=4.9, value=3.0
+    )
+    petal_length = st.number_input(
+        "Petal length (cm)", min_value=0.5, max_value=7.4, value=4.0
+    )
+    petal_width = st.number_input(
+        "Petal width (cm)", min_value=0.1, max_value=3.0, value=2.0
+    )
+
+    submitted = st.form_submit_button("PREDICT")
+
+# st.markdown(submitted)
+
+
+if submitted:
+    payload = {
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width,
+    }
+
+    response = httpx.post(URL, json=payload)
+    st.write(response)
